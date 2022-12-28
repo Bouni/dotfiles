11e3147a774dd8284e1d2bdfef278ec957126bcb
bouni <bouni@owee.de>
2022-12-28 15:07:36 +0000

    add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- Bouni's nvim config, 2022 edition
+--
+-- mostly stolen from Josean Martinez -> https://github.com/josean-dev/dev-environment-files
+--
+require("plugins-setup")
+require("core.options")
+require("core.keymaps")
+require("core.colorscheme")
+require("plugins.comment")
+require("plugins.nvim-tree")
+require("plugins.lualine")
+require("plugins.telescope")
+require("plugins.nvim-cmp")
+require("plugins.lsp.mason")
+require("plugins.lsp.lspsaga")
+require("plugins.lsp.lspconfig")
+require("plugins.lsp.null-ls")
+require("plugins.autopairs")
+require("plugins.treesitter")
+require("plugins.gitsigns")
